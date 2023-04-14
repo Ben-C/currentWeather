ff267fd7fb42fa92ce8079fac6271de0e4afe533
Ben Crowe <sneblot@gmail.com>
2023-04-14 19:40:51 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ if('geolocation' in navigator){
     const api_key = 'd3a29b99f9409f5245af2c98a5bd4b88';
     const weather_url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat +  '&lon=' + lon + '&appid=' + api_key + '&units=metric';
     const weather_response = await fetch(weather_url);
-    document.getElementById('Longitude').textContent = lon;
-    document.getElementById('Latitude').textContent = lat;
     const weather_data = await weather_response.json();
     console.log(weather_data);
+    document.getElementById('Longitude').textContent = lon;
+    document.getElementById('Latitude').textContent = lat;
 });
 }
