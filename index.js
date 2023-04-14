f080c09a0beedf0946b57750f1bfbf8fbdcb07cb
Ben Crowe <sneblot@gmail.com>
2023-04-14 20:27:08 +0000

    updated getCity function

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-require('dotenv').config();
+require("dotenv").config();
 let city;
 let lat;
 let lon;
-if('geolocation' in navigator){
-    navigator.geolocation.getCurrentPosition(async position => {
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(async (position) => {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
-    
+
     const api_key = process.env.API_KEY;
-    const weather_url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat +  '&lon=' + lon + '&appid=' + api_key + '&units=metric';
+    const weather_url =
+      "https://api.openweathermap.org/data/2.5/weather?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=" +
+      api_key +
+      "&units=metric";
     const weather_response = await fetch(weather_url);
     const weather_data = await weather_response.json();
     console.log(weather_data);
-    document.getElementById('Longitude').textContent = lon;
-    document.getElementById('Latitude').textContent = lat;
-});
-};
+    document.getElementById("Longitude").textContent = lon;
+    document.getElementById("Latitude").textContent = lat;
+  });
+}
 
-function getCity {
-    city = document.getElementById('userInput').value;
-    console.log(city);
+function getCity() {
+  city = document.getElementById("userInput").value;
+  console.log(city);
+}
+
+async function geoCode(city, api_key) {
+  const city_weather =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    api_key +
+    "&units=metric";
+  const city_response = await fetch(city_weather);
+  const city_data = await city_response.json();
+  console.log(city_data);
 }
